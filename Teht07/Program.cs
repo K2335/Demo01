87abe8288b5e91cd6e77acd23784c8c530fe94be
K2335 <K2335@student.jamk.fi>
2017-01-19 11:00:52 +0200

    Teht07 done

--- Teht07/Program.cs
+++ Teht07/Program.cs
@@ -10,6 +10,46 @@ namespace Teht07
     {
         static void Main(string[] args)
         {
+            int year;
+            Console.Write("Input year: ");
+            year = int.Parse(Console.ReadLine());
+
+
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                Console.WriteLine("Leap year");
+            }
+            else Console.WriteLine("Not a leap year");
+
+            /*
+            int cent;
+            cent = year % 400;
+            switch(cent)
+            {
+                case 0:
+                    Console.WriteLine("Leap year");
+                    System.Environment.Exit(1);
+                    break;
+            }
+
+            int century;
+            century = year % 100;
+            switch(century)
+            {
+                case 0:
+                    Console.WriteLine("Not a leap year");
+                    System.Environment.Exit(1);
+                    break;
+            }
+
+            
+            int leap;
+            leap = year % 4;
+         
+            if (leap == 0)
+                Console.WriteLine("Leap Year!");
+            else Console.WriteLine("Not a leap year");
+            */
         }
     }
 }
