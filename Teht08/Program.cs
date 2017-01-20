a315ec073b68d1e9352fd12d624817ff639160fc
Ville <K2335@student.jamk.fi>
2017-01-20 15:39:59 +0200

    Teht08 done

--- Teht08/Program.cs
+++ Teht08/Program.cs
@@ -10,7 +10,27 @@ namespace Teht08
     {
         static void Main(string[] args)
         {
+            int num1;
+            int num2;
+            int num3;
 
+            Console.Write("Type first number: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Type second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Type third number: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+                Console.WriteLine(num1 + " is the biggest number");
+            if (num1 > num2 && num1 < num3)
+                Console.WriteLine(num3 + " is the biggest number");
+            if (num1 < num2 && num1 > num3)
+                Console.WriteLine(num2 + " is the biggest number");
+            if (num1 < num2 && num2 < num3)
+                Console.WriteLine(num3 + " is the biggest number");
         }
     }
 }
