4c7c46e068c8eacd6f106da1a17903624f16ec0a
Ville <K2335@student.jamk.fi>
2017-01-20 16:11:43 +0200

    Teht09 done

--- Teht09/Program.cs
+++ Teht09/Program.cs
@@ -10,6 +10,20 @@ namespace Teht09
     {
         static void Main(string[] args)
         {
+            
+            while (true) {
+                int luku;
+                int luku2;
+                Console.Write("Anna luku: ");           
+                luku = int.Parse(Console.ReadLine());
+
+                Console.Write("Anna luku: ");
+                luku2 = int.Parse(Console.ReadLine());
+
+                Console.Write("Tulos on: ");
+                Console.WriteLine(luku + luku2);
+
+            }
         }
     }
 }
