01679cb68e33a69b2a452eeb984405c8411da8a6
Ville <K2335@student.jamk.fi>
2017-01-23 13:18:15 +0200

    Teht18

---
+++ Teht18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sana = "assa";
+            string takaperin = "";
+            for (int i = sana.Length - 1; i >= 0; i--)
+            {
+                takaperin += sana[i];
+            }
+            if (sana == takaperin)
+                Console.Write("Palindromi");
+            else Console.Write("Ei ole palindromi");
+        }
+    }
+}
