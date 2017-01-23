f3151f941d7ba4c1e277145bff815bf2237da569
Ville <K2335@student.jamk.fi>
2017-01-23 12:04:06 +0200

    Teht14 jumped over

---
+++ Teht14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna arvosana: ");
+            int arvo1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Anna arvosana: ");
+            int arvo2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Anna arvosana: ");
+            int arvo3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Anna arvosana: ");
+            int arvo4 = int.Parse(Console.ReadLine());
+
+            Console.Write("Anna arvosana: ");
+            int arvo5 = int.Parse(Console.ReadLine());
+
+            int[] numbers = new[] { arvo1, arvo2, arvo3, arvo4, arvo5 };
+            Console.Write("Nollia: ");
+
+            
+            foreach (var item in numbers)
+                Console.WriteLine("*");
+
+        }
+    }
+}
