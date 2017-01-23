fde6691b74c3e409760e600ff229f8a084e311dc
Ville <K2335@student.jamk.fi>
2017-01-23 12:34:31 +0200

    Teht16 done

---
+++ Teht16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int arpa = rnd.Next(1, 100);
+            int arvaus = 999;
+
+            while (arvaus != arpa) { 
+                Console.Write("Arvaa luku: ");
+                arvaus = int.Parse(Console.ReadLine());
+                if (arvaus < arpa)
+                    Console.WriteLine("Luku on suurempi");
+                if (arvaus > arpa)
+                    Console.WriteLine("Luku on pienempi");
+            }
+            Console.WriteLine("Arvasit oikein!");
+
+        }
+    }
+}
