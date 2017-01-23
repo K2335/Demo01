097ec09494c843054c1683367fef74881201b987
Ville <K2335@student.jamk.fi>
2017-01-23 12:18:39 +0200

    Teht15

--- Teht15/Program.cs
+++ Teht15/Program.cs
@@ -10,6 +10,33 @@ namespace Teht15
     {
         static void Main(string[] args)
         {
+            int count = 0;
+            Console.Write("Anna puun korkeus: ");
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                for (int x = 0; x < a - i - 1; x++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i * 2 + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+            for (int i = 0; i < 2; i++)
+            {
+                for (int x = 0; x < a - count - 1; x++)
+                {
+                    Console.Write(" ");
+
+                }
+                Console.Write("*");
+                Console.WriteLine();
+            }
+            
         }
     }
 }
