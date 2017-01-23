6d62fdbc0249170a73ec708d563b3201ae12cfd7
Ville <K2335@student.jamk.fi>
2017-01-23 12:55:42 +0200

    Teht17 done

---
+++ Teht17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tau1 = { 10, 20, 30 , 40, 50 };
+
+            int[] tau2 = { 5, 15, 25, 35, 45 };
+
+            int[] combined = tau1.Concat(tau2).ToArray();
+
+
+            Array.Sort(combined);
+            foreach (int value in combined)
+            {
+                Console.Write(value);
+                Console.Write(",");
+            }
+           
+        }
+    }
+}
