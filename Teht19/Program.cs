015990e34bd38bd535ec5c12ca035e166418980e
Ville <K2335@student.jamk.fi>
2017-01-23 13:48:14 +0200

    Teht20 done

---
+++ Teht19/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = System.IO.File.ReadAllLines(@"C:\Users\ADMIN\Desktop\Letters\Letters.txt");
+        int LengthOfArray = words.Length;
+        Random rnd = new Random();
+        int random = rnd.Next(1, 3);
+        char[] letters = words[random].ToCharArray();
+        bool WordIsHidden = true;
+        char hiddenChar = '_';
+        char GuessedLetter = hiddenChar;
+
+        var retry = true;
+        while (retry = true)
+        {
+            Console.WriteLine(letters);
+            letters = GuessedLetter.ToString().ToCharArray();
+            for (int i = 1; i <= LengthOfArray; i++)
+            {
+                Console.Write("{0} ", GuessedLetter);
+            }
+            Console.WriteLine("Enter a letter!");
+            char letter = char.Parse(Console.ReadLine());
+            if (words[random].Contains<char>(letter))
+            {
+                WordIsHidden = false;
+                GuessedLetter = letter;
+                Console.Write(letters);
+            }
+            else
+            {
+                if (WordIsHidden == true)
+                {
+                    Console.Write("You guessed wrong!");
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Teht20/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valinta = 0;
+            while (true)
+            {
+                Console.WriteLine("Laskin");
+                Console.WriteLine("Valitse toiminto");
+                Console.WriteLine("0. Lopetus");
+                Console.WriteLine("1. Summa");
+                Console.WriteLine("2. Erotus");
+                Console.WriteLine("3. Tulo");
+                
+
+                valinta = int.Parse(Console.ReadLine());
+                if (valinta == 0)
+                {
+                    System.Environment.Exit(1);
+                }
+                int luku1;
+                int tulos;
+                Console.Write("Anna luku: ");
+                luku1 = int.Parse(Console.ReadLine());
+
+                int luku2;
+                Console.Write("Anna luku: ");
+                luku2 = int.Parse(Console.ReadLine());
+
+             
+                if (valinta == 1)
+                {
+                    tulos = luku1 + luku2;
+                    Console.WriteLine("Vastaus on: " + tulos);
+                }
+                if (valinta == 2)
+                {
+                    tulos = luku1 - luku2;
+                    Console.WriteLine("Vastaus on: " + tulos);
+                }
+                if (valinta == 3)
+                {
+                    tulos = luku1 * luku2;
+                    Console.WriteLine("Vastaus on: " + tulos);
+                }
+              
+            }
+
+        
+
+
+        }
+    }
+}
